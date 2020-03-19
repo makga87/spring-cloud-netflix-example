813e93421a48dc45196a194a0535215087c0a822
hgkim8704 <hgkim8704@gmail.com>
2020-03-19 17:46:18 +0900

    initial commit

---
+++ src/main/java/com/example/eureka/server/SpringCloudNetflixDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudNetflixDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudNetflixDemoApplication.class, args);
+	}
+
+}
